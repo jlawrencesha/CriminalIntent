b1fcadeb03c9fc95fdd831c5fabae058b2f1f5ec
John <jlawrencesha@gmail.com>
2021-01-30 16:50:15 -0800

    pulled out two imports that made no sense.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -13,8 +13,7 @@ import android.widget.CheckBox
 import android.widget.EditText
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResult
-import androidx.fragment.app.setFragmentResultListener
+
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
